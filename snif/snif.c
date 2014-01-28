6c9dc8592ee7d0604392b9385744831fb0e3aa38
hbragge <henri.bragge@gmail.com>
2014-01-28 21:15:20 +0000

    Small update

--- snif/snif.c
+++ snif/snif.c
@@ -1,13 +1,32 @@
 #include <sys/socket.h>
 #include <linux/if_ether.h>
+#include <linux/ip.h>
+#include <arpa/inet.h>
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-void main(void)
+char *ip_proto_label(unsigned char proto)
+{
+	switch (proto) {
+	case 1:
+		return "ICMP";
+	case 2:
+		return "IGMP";
+	case 6:
+		return "TCP";
+	case 17:
+		return "UDP";
+	default:
+		return "Unknown";
+	}
+}
+
+int main(void)
 {
 	int sock, i;
 	struct ethhdr *eh;
+	struct iphdr *iph;
 
 	if ((sock = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL))) < 0) {
 		printf("socket() failed %i\n", errno);
@@ -25,8 +44,13 @@ void main(void)
 		}
 
 		eh = (struct ethhdr *)buf;
+		iph = (struct iphdr *)(buf  + sizeof(struct ethhdr));
+
 		id = ntohs(eh->h_proto);
 		printf("LEN: %i, ID: 0x%X\n", len, id);
+		if (id == 0x800)
+			printf("IP VER: %i, PROTO: %i (%s)\n", iph->version, iph->protocol,
+				ip_proto_label(iph->protocol));
 		printf("SRC:");
 		for (i = 0; i < ETH_ALEN; i++)
 			printf(" %02X", eh->h_source[i]);
@@ -34,9 +58,7 @@ void main(void)
 		for (i = 0; i < ETH_ALEN; i++)
 			printf(" %02X", eh->h_dest[i]);
 
-		if (id != 0x800) {
-			printf("Unrecognized frame ID\n");
-		}
+		printf("\n");
 
 		getchar();
 	}
