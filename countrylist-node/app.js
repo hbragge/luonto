797a456c90336c77a27c94139ad71c06406d57a5
Henri Bragge <henri.bragge@esrlabs.com>
2019-04-30 17:55:40 +0200

    tablefilter
    Change-Id: I906caff05c659fa87306e81e72593e83cc7cdc2a

--- countrylist-node/app.js
+++ countrylist-node/app.js
@@ -1,17 +1,22 @@
 const csv = require('csv-parser'),
       fs = require('fs'),
+      bodyParser = require("body-parser"),
       express = require("express");
 var app = express(),
-    data = {};
+    countries = [];
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
 
 fs.createReadStream('data.txt')
     .pipe(csv())
     .on('data', (row) => {
-        data[row.Country] = {ratio: parseInt(row.Ratio), homicides: parseFloat(row.Homicides), isSpecial: (row.IsSpecial === '1')};
+        countries.push({name: row.Country, ratio: parseInt(row.Ratio), homicides: parseFloat(row.Homicides), isSpecial: (row.IsSpecial === '1')});
     });
 
 app.get("/", (req, res) => {
-    res.send(data);
+    res.render("index", {countries: countries});
 });
 
 app.listen(process.env.PORT, process.env.IP, () => {
