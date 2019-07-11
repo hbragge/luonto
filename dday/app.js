54e7d60f0d85c724f8e670fe440b6c00b4f6bdea
Henri Bragge <henri.bragge@esrlabs.com>
2019-07-11 12:13:25 +0200

    ratioStr

--- dday/app.js
+++ dday/app.js
@@ -1,6 +1,6 @@
 var datejs = require("datejs");
 var dateformat = "MMMM dS, yyyy";
-var ddate = "2025-12-01"
+var ddate = "2025-12-01";
 var startDate = Date.today();
 var ddiff = Date.parse(ddate) - startDate;
 var ddays = ddiff/1000/60/60/24;
@@ -19,19 +19,22 @@ var period = function(dateStr) {
     console.log(date.toString(dateformat) + " --> " +
                 date.addDays(ddays).toString(dateformat));
 }
-var remaining = function(name, dateStr, today) {
-    var remdiff = Date.parse(dateStr) - today;
-    var remdays = remdiff/1000/60/60/24;
-    var bwdate = new Date(today);
-    bwdate.addDays(-remdays);
-    console.log(Math.floor(remdays) +
-                " days (" + round(remdays/30.4, 1) +
-                " months) until " + name + ", backwards: " +
-                bwdate.toString(dateformat));
+var remaining = function(name, dateStr, today, totalMonths) {
+    var remDiff = Date.parse(dateStr) - today;
+    var remDays = remDiff/1000/60/60/24;
+    var remMonths = round(remDays/30.4, 1);
+    var bwDate = new Date(today);
+    var totalMonthsStr = (totalMonths > 0) ? "/" + totalMonths + ".0" : "";
+    var ratioStr = (totalMonths > 0) ? " " + (remMonths/totalMonths * 100).toFixed(2) + "%" : "";
+    bwDate.addDays(-remDays);
+    console.log(Math.floor(remDays) +
+                " days (" + remMonths + totalMonthsStr +
+                " months" + ratioStr + ") until " + name + ", backwards: " +
+                bwDate.toString(dateformat));
 }
 
 dates.forEach(function(d) {
-    remaining(d.name, d.date, startDate);
+    remaining(d.name, d.date, startDate, d.totalMonths || 0);
 });
 console.log("");
 console.log("Same as from:");
